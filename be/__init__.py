e0bd5302f85b6f2bcfd8609f7c1f5e25a3f31a65
maganoegi <platserg94@yahoo.com>
2020-03-01 04:43:21 +0100

    keras and gui added

--- be/__init__.py
+++ be/__init__.py
@@ -11,6 +11,11 @@ import numpy as np
 from PIL import Image, ImageFilter
 from mnist import MNIST
 
+from keras.datasets import mnist
+from keras.models import Sequential, load_model
+from keras.layers.core import Dense, Dropout, Activation
+from keras.utils import np_utils
+
 THRESHOLD = 100 # grey
 SIZE = 28
 
@@ -35,7 +40,71 @@ def print_MNIST_digit(line_img):
 
 def train(images, labels):
     print("\n------------------- TRAINING -------------------\n")
+    (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+    # building the input vector from the 28x28 pixels
+    X_train = X_train.reshape(60000, 784)
+    X_test = X_test.reshape(10000, 784)
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
 
+    # normalizing the data to help with the training
+    X_train /= 255
+    X_test /= 255
+
+    print(np.unique(Y_train, return_counts=True))
+    # one-hot encoding using keras' numpy-related utilities
+    n_classes = 10
+    print("Shape before one-hot encoding: ", Y_train.shape)
+    Y_train = np_utils.to_categorical(Y_train, n_classes)
+    Y_test = np_utils.to_categorical(Y_test, n_classes)
+    print("Shape after one-hot encoding: ", Y_train.shape)
+
+    # building a linear stack of layers with the sequential model
+    model = Sequential()
+    model.add(Dense(512, input_shape=(784,)))
+    model.add(Activation('relu'))                            
+    model.add(Dropout(0.2))
+
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.2))
+
+    model.add(Dense(10))
+    model.add(Activation('softmax'))
+
+    # compiling the sequential model
+    model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
+
+    # training the model and saving metrics in history
+    history = model.fit(X_train, Y_train,
+            batch_size=128, epochs=20,
+            verbose=2,
+            validation_data=(X_test, Y_test))
+
+    # saving the model
+    parentDirectory = os.path.dirname(os.path.abspath(__file__))
+    save_dir = parentDirectory + "/results/"
+    model_name = 'keras_mnist.h5'
+    model_path = os.path.join(save_dir, model_name)
+    model.save(model_path)
+    print('Saved trained model at %s ' % model_path)
+
+    mnist_model = load_model(model_path)
+    loss_and_metrics = mnist_model.evaluate(X_test, Y_test, verbose=2)
+
+    print("Test Loss", loss_and_metrics[0])
+    print("Test Accuracy", loss_and_metrics[1])
+
+    # load the model and create predictions on the test set
+    mnist_model = load_model(model_path)
+    predicted_classes = mnist_model.predict_classes(X_test)
+
+    # see which we predicted correctly and which not
+    correct_indices = np.nonzero(predicted_classes == Y_test)[0]
+    incorrect_indices = np.nonzero(predicted_classes != Y_test)[0]
+    print()
+    print(len(correct_indices)," classified correctly")
+    print(len(incorrect_indices)," classified incorrectly")
 
 
 def MNIST_convert():
@@ -84,12 +153,18 @@ if __name__ == '__main__':
     # Extract images and labels
     mndata, images, labels = MNIST_extract(data_root)
 
-    # Display individual digit in terminal
-    index = random.randrange(0, len(images))
-    img = images[index]
+    train(images, labels)
 
-    print_MNIST_digit(img)
+
+    # Display individual digit in terminal
+    # index = random.randrange(0, len(images))
+    # img = images[index]
+
+    # print_MNIST_digit(img)
 
     # Test MNIST conversion
-    custom = MNIST_convert()
-    print_MNIST_digit(custom)
+    # data = MNIST_convert()
+    # print_MNIST_digit(custom)
+
+
+
--- fe/__main__.py
+++ fe/__main__.py
@@ -1,35 +1,177 @@
 
 import sys
-from PyQt5.QtWidgets import QWidget, QMainWindow, QLabel, QApplication
-from PyQt5.QtGui import QPainter, QColor, QPen, QPixmap
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QMainWindow, QLabel, QApplication, QVBoxLayout, QHBoxLayout, QPushButton, QSlider, QFileDialog
+from PyQt5.QtGui import QPainter, QColor, QPen, QPixmap, QImage, QFont
+from PyQt5.QtCore import Qt, QSize
 
 # https://app.getpocket.com/read/928938439
-class MainWindow(QMainWindow):
-    
+
+class Canvas(QLabel):
     def __init__(self):
         super().__init__()
-
-        self.setGeometry(500, 300, 1000, 600)
-        self.setWindowTitle('OCR Application -- by Sergey Platonov')
-        self.show()
+        pixmap = QPixmap(500, 500)
+        self.setPixmap(pixmap)
+        self.penWidth = 5
         
-        self.label = QLabel
-        canvas = QPixmap(500, 500)
-        self.label.setPixmap(canvas)
-        self.setCentralWidget(self.label)
-        self.draw()
+        self.last_x, self.last_y = None, None
 
+        self.pixmap().fill(QColor(Qt.white))
 
-    def draw(self):
-        painter = QPainter(self.label.pixmap())
-        painter.drawLine(10, 10, 300, 200)
+    def set_pen_color(self, c):
+        self.pen_color = QColor(c)
+
+    def mouseMoveEvent(self, e):
+        if self.last_x is None: # First event.
+            self.last_x = e.x()
+            self.last_y = e.y()
+            return # Ignore the first time.
+
+        painter = QPainter(self.pixmap())
+        p = painter.pen()
+        p.setWidth(self.penWidth)
+        p.setColor(QColor("#000"))
+        # painter.drawRect(600, 0, 1000, 600)
+        # painter.fillRect(QColor=QColor('#ffffff'))
+        painter.setPen(p)
+        painter.drawLine(self.last_x, self.last_y, e.x(), e.y())
         painter.end()
+        self.update()
+
+        # Update the origin for next time.
+        self.last_x = e.x()
+        self.last_y = e.y()
+
+    def mouseReleaseEvent(self, e):
+        self.last_x = None
+        self.last_y = None
+    
+    def changePenWidth(self, width):
+        self.penWidth = width
+
+    def reset(self):
+        self.pixmap().fill(QColor(Qt.white))
+        self.update()
+
+    def save_image(self):
+        pixmap = self.pixmap()
+        pixmap.save("Test.png", "PNG")
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("OCR")
+        self.canvas = Canvas()
+        self.slider = QSlider(Qt.Horizontal)
+        self.submitBtn = self.add_btn("Submit")
+        self.resetBtn = self.add_btn("Erase")
+        self.labels = [self.add_lbl(str(i)) for i in range(10)]
+        self.lbl_result = self.add_lbl("Result Test")
+
+        w = QWidget()
+        l = QVBoxLayout()
+        l_container = QHBoxLayout()
+        lhv = QVBoxLayout()
+        lhvv = QVBoxLayout()
+        lhh = QHBoxLayout()
+        lhh = QVBoxLayout()
+
+        l_container.addLayout(lhv)
+        l_container.addLayout(lhvv)
+        l_container.addLayout(lhh)
+
+        w.setLayout(l)
+        l.addWidget(self.canvas)
+        l.addWidget(self.slider)
+        lhv.addWidget(self.submitBtn)
+        lhv.addWidget(self.resetBtn)
+        for i in range(10):
+            lhvv.addWidget(self.labels[i])
+        lhh.addWidget(self.lbl_result)
+
+
+        l.addLayout(l_container)
+
+        palette = QHBoxLayout()
+
+        l.addLayout(palette)
+        self.setStyleSheet("background-color: #123456;")  
+
+        self.setCentralWidget(w)
+
+        self.submitBtn.clicked.connect(self.canvas.save_image)
+        self.resetBtn.clicked.connect(self.canvas.reset)
+        
+        self.slider.setMinimum(5)
+        self.slider.setMaximum(35)
+        self.slider.valueChanged.connect(self.updateThicknessVal)
+    
+        for index, label in enumerate(self.labels):
+            self.write_lbl_val(label, float(index + 1), index) 
+        
+        self.print_result_lbl(9, asPredicted=False)
+
+
+    def updateThicknessVal(self):
+        val = self.slider.value()
+        self.canvas.changePenWidth(val)
+
+    def add_lbl(self, text):
+        lbl = QLabel()
+        lbl.setText(text)
+        return lbl
+
+    def add_btn(self, text):
+        btn = QPushButton()
+        btn.setText(text) 
+        return btn
+
+    def write_lbl_val(self, label, val, index):
+        spaces = lambda x: " " * x
+        rounded_2_half = round(val * 2) / 2
+        increments = int(rounded_2_half / 0.5)
+        label.setText(spaces(6) + str(index) + spaces(2) + ("|" * int(increments)))
+
+    def print_result_lbl(self, val, asPredicted):
+        red = 255 if asPredicted else 0
+        green = 0 if asPredicted else 255
+        self.lbl_result.setStyleSheet(f"color: rgb({red}, {green}, 0);")
+        self.lbl_result.setText(str(val))
+        self.lbl_result.setFont(QFont("Arial", 70, QFont.Bold))
+
+
+
+
+
+
+
+
+
+# class MainWindow(QMainWindow):
+    
+#     def __init__(self):
+#         super().__init__()
+
+#         self.setGeometry(500, 300, 1000, 600)
+#         self.setWindowTitle('OCR Application -- by Sergey Platonov')
+#         self.show()
+        
+#         self.label = QLabel
+#         canvas = QPixmap(500, 500)
+#         self.label.setPixmap(canvas)
+#         self.setCentralWidget(self.label)
+#         self.draw()
+
+
+#     def draw(self):
+#         painter = QPainter(self.label.pixmap())
+#         painter.drawLine(10, 10, 300, 200)
+#         painter.end()
         
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    mw = MainWindow()
-    mw.show()
+    window = MainWindow()
+    window.show()
     sys.exit(app.exec_())
